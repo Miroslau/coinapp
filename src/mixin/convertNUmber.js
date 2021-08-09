c3f5d6e778770975ddacd2c8b234975bf246c31d
Miroslau <mira2408@mail.ru>
2021-08-09 21:12:27 +0300

    add dist

--- src/mixin/convertNUmber.js
+++
@@ -1,8 +0,0 @@
-export default {
-    methods: {
-        convertNumber: function (number) {
-            let num = Math.round(+number * 100) / 100
-            return String(num.toFixed(2)).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
-        }
-    }
-}
